cfb6bac4f46bb4b729dacdd2540739ab99b1b1cc
fmoney90 <32916232+fmoney90@users.noreply.github.com>
2017-11-24 19:58:20 -0600

    Add files via upload

---
+++ BERNARD YME UNIDAD 3/BYME_Animalfactory.py
@@ -0,0 +1,40 @@
+
+
+
+"""    BERNARD Yves Mary Emmanuel
+       CODE : 1215101007
+       Factory Exemple
+"""
+
+
+class Dog:
+	"""A simple  Dog class"""
+	def __init__(self, name):
+		self._name = name
+
+	def speak(self):
+		return "Woof!"
+
+class Cat:
+	"""A simple Cat class"""
+	def __init__(self, name):
+		self._name = name
+
+	def speak(self):
+		return "Meaw!"
+
+def get_pet(pet="dog"):
+	"""the factory method"""
+
+	pets =dict(dog=Dog("Hope"), cat=Cat("Peace"))
+	return pets[pet]
+
+d = get_pet("dog")
+print(d.speak())
+
+c = get_pet("cat")
+print(c.speak())
+
+
+
+
